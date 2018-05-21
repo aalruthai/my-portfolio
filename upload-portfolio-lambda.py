aab780a78010b0e69f35809596dfd645c79a748c
Amr ALruthai <aalruthai@kacst.edu.sa>
2018-05-21 11:08:33 +0300

    Updated Lambda FUnction added exception handling and SNS

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -4,20 +4,27 @@ import zipfile
 import mimetypes
 
 def lambda_handler(event, context):
+    sns = boto3.resource('sns')
+    topic = sns.Topic('arn:aws:sns:us-east-1:243485805400:deployPortfolioTopic')
+    try:
 
 
-    s3 = boto3.resource('s3')
-    portfolio_bucket = s3.Bucket('portfolio.amr')
+        s3 = boto3.resource('s3')
+        portfolio_bucket = s3.Bucket('portfolio.amr')
 
-    build_bucket = s3.Bucket('portfoliobuild.amr')
+        build_bucket = s3.Bucket('portfoliobuild.amr')
 
-    portfolio_zip = BytesIO()
-    build_bucket.download_fileobj('portfoliobuild.zip',portfolio_zip)
+        portfolio_zip = BytesIO()
+        build_bucket.download_fileobj('portfoliobuild.zip',portfolio_zip)
 
-    with zipfile.ZipFile(portfolio_zip) as myzip:
-        for nm in myzip.namelist():
-            obj = myzip.open(nm)
-            portfolio_bucket.upload_fileobj(obj,nm, ExtraArgs={'ContentType':mimetypes.guess_type(nm)[0]})
-            portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
-
+        with zipfile.ZipFile(portfolio_zip) as myzip:
+            for nm in myzip.namelist():
+                obj = myzip.open(nm)
+                portfolio_bucket.upload_fileobj(obj,nm, ExtraArgs={'ContentType':mimetypes.guess_type(nm)[0]})
+                portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+        print("Job Done!")
+        topic.publish(Subject="Portfolio Depolyed",Message="Portfolio deployed successfully")
+    except:
+        topic.publish(Subject="Portfolio Deployment Failed",Message="The Portfolio Deployement Failed")
+        raise
     return 'Hello from Lambda'
