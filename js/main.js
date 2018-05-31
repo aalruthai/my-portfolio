03731a59738acf5755af1f58719249dae9345fe7
Amr ALruthai <aalruthai@kacst.edu.sa>
2018-05-31 18:28:06 +0300

    created firs 2 react components

--- js/main.js
+++ js/main.js
@@ -1,3 +1,31 @@
-import 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ExampleWork from './example-work';
 
-console.log("Loaded react-dom");
+const myWork = [
+  {
+    'title':"Work Example",
+    'image':{
+      'desc':"example screenshot of a project involving code",
+      'src':"images/example5.png",
+      'comment':""
+    }
+  },
+  {
+    'title':"Boilerplate Example!!!",
+    'image':{
+      'desc':"Serverless Portfolio",
+      'src':"images/example4.png",
+      'comment':""
+    }
+  },
+  {
+    'title':"Work ExampleS",
+    'image':{
+      'desc':"example screenshot of a project involving cats",
+      'src':"images/example6.png",
+      'comment':""
+    }
+  }
+]
+ReactDOM.render(<ExampleWork work={myWork}/>,document.getElementById('example-work'));
